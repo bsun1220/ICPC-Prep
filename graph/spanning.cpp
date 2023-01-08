b627ccb13923f53f6d1fd936ce3f3646c9b1f416
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2023-01-08 15:55:53 -0500

    spanning

---
+++ graph/spanning.cpp
@@ -0,0 +1,30 @@
+#include <set>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <deque>
+#include <stack>
+#include <queue>
+using namespace std;
+
+const int N = 5;
+vector<vector<int>> edge_list;
+
+void kruskal();
+
+int main(){
+
+}
+
+void kruskal(){
+    sort(edge_list.begin(), edge_list.end());
+    set<int> nodes;
+    for (int i = 0; i < edge_list.size(); i++){
+        vector<int> list = edge_list[i];
+        bool found1 = nodes.count(edge_list[1]);
+        bool found2 = nodes.count(edge_list[2]);
+        if(!found1 && ! found2){
+            continue;
+        }
+    }
+}
