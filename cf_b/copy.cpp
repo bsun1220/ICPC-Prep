91c9adaf9b3b8f87010e5b8f8ce0c1e40f05b702
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2023-01-19 01:42:31 -0500

    copy

--- cf_b/copy.cpp
+++ cf_b/copy.cpp
@@ -11,6 +11,14 @@ int main(){
     int t;
     cin >> t;
     for(int test = 0; test < t; test++){
-        
+        long length;
+        set<long> s; 
+        cin >> length;
+        for(long i = 0; i < length; i++){
+            long val;
+            cin >> val;
+            s.insert(val);
+        }
+        cout << s.size() << endl;
     }
 }
